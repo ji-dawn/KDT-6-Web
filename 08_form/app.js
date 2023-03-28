75c2f97b221bc5feba0c50d74f6252e0a280532a
Jihyo-Android <wlgy33@naver.com>
2023-03-28 19:56:42 +0900

    study: get/post form 전송

--- 08_form/app.js
+++ 08_form/app.js
@@ -6,6 +6,8 @@ const PORT = 8000;
 // view engine에 ejs 등록
 app.set("view engine", "ejs");
 app.use("/views", express.static(__dirname + "/views")); // ejs를 담을 views 폴더 경로 설정
+app.use(express.urlencoded({ extended: true })); // post 요청으로 들어오는 모든 형식의 데이터를 파싱
+app.use(express.json()); // json 형식로 데이터를 주고 받음
 
 // routing : 경로 설정
 // 브라우저에서 어떤 url로 접속했을 때 어떤 페이지를 보여줄 것인가
@@ -13,7 +15,21 @@ app.use("/views", express.static(__dirname + "/views")); // ejs를 담을 views 
 // ex) localhost:PORT/ 접속했을 때, index.ejs를 보여줌
 app.get("/", function (req, res) {
   // views/index.ejs 파일을 찾아 응답/렌더링
-  res.render("index");
+  const myTitle = "폼 실습을 ~~";
+  // res.render("index", { title: "폼 실습을 해보자" });
+  res.render("index", { title: myTitle, name: "Ji" });
+});
+
+// Front에서 submit되면
+app.get("/getForm", function (req, res) {
+  // console.log(req);
+  console.log(req.query); // { id: 'wlgy33', pw: 'sdkjfsj' }
+  res.send("get 요청 성공");
+});
+
+app.post("/postForm", function (req, res) {
+  console.log(req.body);
+  res.send("post 요청 성공");
 });
 
 app.listen(PORT, function () {
