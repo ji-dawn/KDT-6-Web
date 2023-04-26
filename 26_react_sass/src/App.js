ab955b0b58bccc17f5fb3ff249848e689725d957
ji-dawn <wlgy33@naver.com>
2023-04-26 18:48:53 +0900

    study: sass 실습

---
+++ 26_react_sass/src/App.js
@@ -0,0 +1,21 @@
+import CssComponent from "./CssComponent";
+import SassComponent from "./SassComponent";
+import Larva from "./Larva";
+import MixInComponent from "./MixInComponent";
+import Prob4 from "./Prac4";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h2>CssComponent</h2>
+      <CssComponent />
+      <h2>SassComponent</h2>
+    <SassComponent /> 
+      <Larva /> */}
+      {/* <MixInComponent /> */}
+      <Prob4 />
+    </div>
+  );
+}
+
+export default App;
